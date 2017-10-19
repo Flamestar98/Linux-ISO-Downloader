90f99514eedc6bd80193adaadedebd4be800693b
Flamestar98 <Flamestar98@users.noreply.github.com>
2017-10-19 17:52:28 -0400

    Update git_linux-iso-downloader.sh

--- git_linux-iso-downloader.sh
+++ git_linux-iso-downloader.sh
@@ -1,13 +1,17 @@
 # Message
+echo ""
 echo "Checking for Git install..."
 # Check for Git
 if which git >/dev/null; then
 # Message
+    echo ""
     echo "Git installed"
 # Git not installed
 else
 # Message
+    echo ""
     echo "Git is required to run this script. Would you like to install Git now?"
+    echo ""
 # Install Git?
     select GIT in Yes No
     do
@@ -16,26 +20,34 @@ else
                     break
                     ;;
             *)
+                    echo ""
                     echo "Please select yes or no" 
+                    echo ""
                     ;;
             esac
     done
 # Install Git
     if [[ $GIT = "Yes" ]]; then
 # Message
+        echo ""
         echo "Installing Git..."
+        echo ""
 # Update APT and install Git
         sudo apt update && sudo apt install git
     fi
 # Don't install Git
     if [[ $GIT = "No" ]]; then
 # Message
+        echo ""
         echo "Goodbye"
+        echo ""
     fi
 fi
 
 # Replace?
+ehco ""
 echo 'Would you like to replace your current version of Linux ISO Downloader with the most recent version on Github?'
+echo ""
 select REPLACE in Yes No
 do
         case $REPLACE in 
@@ -43,7 +55,9 @@ do
                 break
                 ;;
         *)
-                echo "Please select yes or no" 
+                echo ""
+                echo "Please select yes or no"
+                echo ""
                 ;;
         esac
 done
@@ -51,7 +65,9 @@ done
 # Replace
 if [[ $REPLACE = "Yes" ]]; then
 # Message
+    echo ""
     echo "Deleting your current version of Linux ISO Downloader and downloading most recent version from Github..."
+    echo ""
 # Remove current L-I-D and clone Git
     cd ~
     sudo rm -r Linux-ISO-Downloader && git clone https://github.com/Flamestar98/Linux-ISO-Downloader.git
@@ -59,5 +75,7 @@ fi
 # End
 if [[ $REPLACE = "No" ]]; then
 # Message
+    echo ""
     echo "Goodbye"
+    echo ""
 fi
