8fc79c6c5e2f17ff07d1d6d9bb579a6df219585d
Flamestar98 <easthighNerd@protonmail.com>
2018-02-06 20:52:12 -0500

    Updates Kali Linux to 2018.1 and Qubes 4.0 to RC4

--- tux-disc_1.9-2.sh
+++ tux-disc_1.9-2.sh
@@ -413,7 +413,7 @@ fi
 if [[ $DISTRO = "Qubes" ]]; then
 # Release
     echo "Now select a release for $DISTRO"
-    select RELEASE in "3.2" "4.0-rc2"
+    select RELEASE in "3.2" "4.0-rc4"
     do
             case $RELEASE in
             "3.2"|"4.0-rc2")
@@ -436,7 +436,7 @@ if [[ $DISTRO = "Qubes" ]]; then
 # Message
             FINAL=$"$DISTRO $RELEASE 64-bit"
 # Download
-            OPTION=$"-P" URL=$" https://mirrors.kernel.org/qubes/iso/Qubes-R4.0-rc2-x86_64.iso"
+            OPTION=$"-P" URL=$" https://mirrors.kernel.org/qubes/iso/Qubes-R4.0-rc4-x86_64.iso"
     fi
 fi
 # Ubuntu
