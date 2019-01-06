0f3fe4a086f795e7051f3beb3db285b654c297d0
Flamestar98 <johnglaufert@pm.me>
2019-01-06 01:32:41 -0500

    Added the ability to remove files from older versions, and script output

--- build.sh
+++ build.sh
@@ -5,14 +5,21 @@ if [ $USER = "root" ]; then
 	cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 
 	if [ -d /usr/lib/tux-disc ]; then
-		rm -r /usr/lib/tux-disc
+		echo 'Removing old version...'
+		rm -r /usr/lib/tux-disc && echo 'Old version removed!' || echo 'Old version not removed :('
 	fi
 
 	mkdir /usr/lib/tux-disc
 
 	cp -r ./ /usr/lib/tux-disc
 
-	ln -s /usr/lib/tux-disc/tux-disc_2.0-1.sh /usr/bin/tux-disc && chmod +x /usr/bin/tux-disc
+	if [ -e /usr/bin/tux-disc ]; then
+		echo 'Removing old link...'
+		rm /usr/bin/tux-disc && echo 'Old link removed!' || echo 'Old link not removed :('
+	fi
+
+	echo 'Linking script into /usr/bin...'
+	ln -s /usr/lib/tux-disc/tux-disc_2.0-1.sh /usr/bin/tux-disc && chmod +x /usr/bin/tux-disc && echo 'Linking complete!' || echo 'Linking failed :('
 else
 	echo 'This build script must be run as root. Please either login as root or use sudo'
 fi
