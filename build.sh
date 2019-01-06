d8dff74d43102080222cf2b9abc83d2007cf8952
Flamestar98 <johnglaufert@pm.me>
2019-01-06 01:45:39 -0500

    Updated to link desktop file

--- build.sh
+++ build.sh
@@ -20,6 +20,14 @@ if [ $USER = "root" ]; then
 
 	echo 'Linking script into /usr/bin...'
 	ln -s /usr/lib/tux-disc/tux-disc_2.0-1.sh /usr/bin/tux-disc && chmod +x /usr/bin/tux-disc && echo 'Linking complete!' || echo 'Linking failed :('
+
+	if [ -e /usr/share/applications/tux-disc.desktop ]; then
+		echo 'Removing old desktop file...'
+		rm /usr/share/applications/tux-disc.desktop && echo 'Old desktop file removed!' || echo 'Old desktop file not removed :('
+	fi
+
+	echo 'Linking desktop file...'
+	ln -s /usr/lib/tux-disc/tux-disc.desktop /usr/share/applications/tux-disc.desktop && echo 'Linking complete!' || echo 'Linking failed :('
 else
 	echo 'This build script must be run as root. Please either login as root or use sudo'
 fi
